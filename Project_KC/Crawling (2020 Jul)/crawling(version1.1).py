6a7d46fc34857fa1027bfed356c6952d60dc98ce
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2020-08-26 23:30:26 +0900

    update (sales summary)

--- Project_KC/Crawling (2020 Jul)/crawling(version1.1).py
+++ Project_KC/Crawling (2020 Jul)/crawling(version1.1).py
@@ -19,19 +19,26 @@ from openpyxl import Workbook
 
 
 # ChromeDriver 파일 위치 변경 필요
-driver = webdriver.Chrome(r"D:\Git\chromedriver.exe")
+driver = webdriver.Chrome(r"C:\Users\Admin\Google Drive\KyoChon Sales Data\chromedriver.exe")
 
 driver.get('http://kyochon.revelup.com')
 time.sleep(5)
 revel_id = "gyutae"
 driver.find_element_by_xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div/div/div[1]/div/input').send_keys(revel_id)
 print()
-passwd = input("password for {}: ".format(revel_id))
+passwd = "Teddygom850212!"
 print()
 driver.find_element_by_xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div/div/div[2]/div[1]/div/input').send_keys(passwd)
 driver.find_element_by_xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/button').click()
 time.sleep(5)
-
+driver.find_element_by_xpath('//*[@id="navigation"]/div/ul/li[2]/a/span').click()
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
+time.sleep(2)
 
 # sales 추출 관련 function
 def sto_summary():
@@ -43,25 +50,17 @@ def sto_summary():
     # Time
     now_hour = str(now.hour)
     # Net sales
-    #acc_sales = driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/div/div[2]').text
-    # net sales (taxable)
-    driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
-    time.sleep(1)
-    driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
-    time.sleep(1)
-    driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/ul/li[1]/div/div[1]').click()
-    time.sleep(1)
-    acc_sales = driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/ul/li[1]/ul/li[1]/div/div[2]').text
-
+    acc_sales = driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[1]/ul/li[1]/ul/li[1]/ul/li[1]/ul/li[1]//ul/li[1]/div/div[2]').text
     # Net transaction
     acc_transaction = driver.find_element_by_xpath('//*[@id="table_data_place"]/div[1]/div[2]/ul/li[1]/ul/li[1]/div/div[2]').text
-    # avg check (calculated)
+    # avg check (calculated)]
     try:
         avg_check = float(acc_sales)/float(acc_transaction)
     except ZeroDivisionError:
         avg_check = 0
+        
     table1 = pd.DataFrame([sto, now_date, now_hour, acc_sales, acc_transaction, avg_check], 
-                          index = ['Store', 'Date', 'Hour', 'Sales', 'Transaction', 'Avg. check'],
+                          index = ['Store', 'Date', 'Hour', 'Sales', 'Transaction','avg check'],
                           columns = ['Summary'])
     table1 = table1.reset_index()
     return table1
@@ -94,17 +93,22 @@ for i in range(n_sto):
         driver.find_element_by_xpath('//*[@id="header"]/div[2]/div[5]/div[2]/span').click()
         time.sleep(5)
         driver.find_element_by_xpath('//*[@id="establishments-tree"]/div/div[2]/div[2]/div[2]/span[1]').click()
-    time.sleep(3)
+    time.sleep(5)
     driver.find_elements_by_class_name('fancytree-title.disabled')[i].click()
-    time.sleep(7)
+    time.sleep(5)
     driver.find_element_by_xpath('//*[@id="navigation"]/div/ul/li[2]/a/span').click()
-    time.sleep(10)
+    time.sleep(5)
     summ.append(sto_summary())
     driver.find_element_by_xpath('//*[@id="report_sub_nav"]/li[4]/a').click()
-    time.sleep(10)
+    time.sleep(5)
     range_mix.append(prod_mix())
     
     print('{} finished'.format(driver.find_element_by_xpath('//*[@id="header"]/div[2]/div[5]/div[2]/span').text))
+
+
+sales_summ = pd.DataFrame()
+for i in range(len(summ)):
+    sales_summ = pd.concat([sales_summ, pd.DataFrame([summ[i].iloc[0,1],summ[i].iloc[3,1]]).T])
 
 
 # excel에 저장
@@ -121,11 +125,14 @@ for i in range(len(summ)):
     for r in range(nrow):
         for c in range(ncol):
             ws.cell(r+1,c+4, value = range_mix[i].iloc[r,c])
+ws = wb.create_sheet('summary')
+ws.cell(1,1, value = summ[0].iloc[1,1])
+nrow, ncol = sales_summ.shape
+for r in range(nrow):
+    for c in range(ncol):
+        ws.cell(r+1,c+1, value = summ[i].iloc[r,c])
 wb.remove(wb['Sheet'])
 
 now = datetime.datetime.now()
-fname = "Summary {y}{m}{d}.xlsx".format(y=now.year, m=('0' + str(now.month))[-2:], d=('0' + str(now.day))[-2:])
+fname = "Closing {y}{m}{d}{h}.xlsx".format(y=now.year, m=('0' + str(now.month))[-2:], d=('0' + str(now.day))[-2:],h=now.hour)
 wb.save(fname)
-
-os.startfile(fname)
-
